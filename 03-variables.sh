0b0260c3bd5be49d2153a94f2ef3649667824fab
Chandra Mouli <kcmchandu18@gmail.com>
2026-06-21 02:09:29 -0400

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/blash
+PERSON1=RAMESH
+PESRSON2=SURESH
+
+echo "Ramesh: Hi Suresh, How are your doing?"
+echo "suresh: Hello Ramesh. I am fine. how are doing?"
+echo "Ramesh: I am doing good suresh. What's going on?"
+echo "suresh: I started learing shell script ramesh"
