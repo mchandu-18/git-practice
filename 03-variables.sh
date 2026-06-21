9b279a4885aa8671b83043a8a63359da79a9199c
Chandra Mouli <kcmchandu18@gmail.com>
2026-06-21 03:25:29 -0400

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Mouli
+PERSON2=Sarevesh
 
 echo "$PERSON1: Hi $PERSON2, How are your doing?"
-echo "$PERSON2: Hello Ramesh. I am fine. how are doing?"
+echo "$PERSON2: Hello $PERSON1. I am fine. how are doing?"
 echo "$PERSON1: I am doing good $PERSON2. What's going on?"
 echo "$PERSON2: I started learing shell script $PERSON1"
