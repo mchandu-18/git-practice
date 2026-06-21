515a3f3ec8502beea1fb1cd3cba9cc4436c30e2e
Chandra Mouli <kcmchandu18@gmail.com>
2026-06-21 01:49:36 -0400

    add converstation script

---
+++ 02- converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are your doing?"
+echo "suresh: Hello Ramesh. I am fine. how are doing?"
+echo "Ramesh: I am doing good suresh. What's going on?"
+echo "suresh: I started learing shell script ramesh"
