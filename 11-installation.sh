adbc65f5db5a6624acf7e9e25d89df7215757fb9
Chandra Mouli <kcmchandu18@gmail.com>
2026-06-23 15:17:38 -0400

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,9 +3,18 @@
 USERID=$(id -u)
 #echo "user ID is: $USERID"
 
-if[ $USERID -ne 0]
+if [ $USERID -ne 0]
 then
     echo "please run this script with root priveleges"
     exit 1
 fi
 
+dnf list installed git 
+
+if [$? -ne 0]
+then
+    echo "Git is not installed, going to install it .."
+
+else  
+    echo "Git is already installed, noting to do.."
+fi 
