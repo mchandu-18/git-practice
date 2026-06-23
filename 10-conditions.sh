a0643a82e084a916bb51fb40a63bdf0469292f65
Chandra Mouli <kcmchandu18@gmail.com>
2026-06-23 13:52:45 -0400

    variables

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,7 +6,7 @@ if [ $Number -gt ] #gt, 1t, eq, -ne, -ge, -le
 then
     echo "Given number : $NUMBER is greater than 20"
 else
-    eco "Given number: $NUMBER is less than 20"
+    echo "Given number: $NUMBER is less than 20"
 fi 
 
  
