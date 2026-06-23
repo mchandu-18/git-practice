6c3ee35c7f34d3a7dd7ff11c49366695123891c8
Chandra Mouli <kcmchandu18@gmail.com>
2026-06-23 13:20:19 -0400

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "All variables passed to the script:$@"
+echo "All variables passed to the script: $@"
 
-echo "Number of variables passed:$@"
+echo "Number of variables passed: $@"
 
 echo "script name: $0"
 
-echo "Current working directory:PWD
+echo "Current working directory: PWD"
 
+echo "home directory of current user: $HOME"
 
